1bf82f641621e0b2db56434d3602f1d7ba84be28
Giampiero Bono <giampiero.bono@hotmail.it>
2019-12-29 21:05:53 +0100

    feat(common-components): add property card skeleton to be displayed on properties list loading

--- packages/ui-common-components/src/index.ts
+++ packages/ui-common-components/src/index.ts
@@ -1,2 +1,3 @@
-export * from "./property-card";
 export * from "./properties-list";
+export * from "./property-card";
+export * from "./property-card-skeleton";
---
+++ packages/ui-common-components/src/property-card-skeleton/property-card-skeleton.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./property-card-skeleton.component.scss";
+
+export const PropertyCardSkeletonComponent = (): React.ReactElement => {
+  const skeletonCards = [];
+  for (let i = 0; i < 10; i++) {
+    skeletonCards.push(<div className="card-skeleton mb-3" />);
+  }
+  return (
+    <div className="row">
+      <div className="col">{skeletonCards}</div>
+    </div>
+  );
+};
