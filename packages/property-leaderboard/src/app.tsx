5a0775829b2a3a5e10c0f38ff15aecbb906a65fe
Giampiero Bono <giampiero.bono@hotmail.it>
2019-12-30 10:30:06 +0100

    feat(property-leaderboard): change PropertiesListContainer component to use hooks instead of connect

--- packages/property-leaderboard/src/app.tsx
+++ packages/property-leaderboard/src/app.tsx
@@ -1,32 +1,16 @@
-import { PropertiesListComponent } from "@vrbo/common-components";
-import { GlobalState, loadPropertiesAction } from "@vrbo/store";
-import { pathOr } from "ramda";
 import * as React from "react";
-import { connect, Provider } from "react-redux";
+import { Provider } from "react-redux";
+import { PropertiesListContainer } from "./components";
 import { store } from "./state";
 
-const mapStateToProps = (state: GlobalState) => {
-  return {
-    properties: pathOr([], ["propertiesList", "properties"])(state),
-    isPropertiesListLoading: state.apiCallStatus.propertiesPollLoading
-  };
-};
-
-const PropertiesListContainer = connect(mapStateToProps)(
-  PropertiesListComponent
-);
-store.dispatch(loadPropertiesAction());
-
-export function App() {
-  return (
-    <Provider store={store}>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <PropertiesListContainer />
-          </div>
+export const App = () => (
+  <Provider store={store}>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <PropertiesListContainer />
         </div>
       </div>
-    </Provider>
-  );
-}
+    </div>
+  </Provider>
+);
---
+++ packages/property-leaderboard/src/components/properties-list/properties-list-container.component.tsx
@@ -0,0 +1,23 @@
+import { PropertiesListComponent } from "@vrbo/common-components";
+import {
+  getPropertiesList,
+  loadPropertiesAction,
+  getPropertiesListLoadingStatus
+} from "@vrbo/store";
+import React, { useEffect } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+
+export const PropertiesListContainer = () => {
+  const dispatch = useDispatch();
+  const properties = useSelector(getPropertiesList, shallowEqual);
+  const isLoading = useSelector(getPropertiesListLoadingStatus, shallowEqual);
+  useEffect(() => {
+    dispatch(loadPropertiesAction());
+  }, [dispatch]);
+  return (
+    <PropertiesListComponent
+      properties={properties}
+      isPropertiesListLoading={isLoading}
+    />
+  );
+};
