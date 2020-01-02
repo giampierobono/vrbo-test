317936dca456054e4f53973206e350f788904615
Giampiero Bono <giampiero.bono@hotmail.it>
2020-01-02 12:23:08 +0100

    fix(common-components): add key attribute to selectbox options

--- packages/ui-common-components/src/sortby-selectbox/sortby-selectbox.component.tsx
+++ packages/ui-common-components/src/sortby-selectbox/sortby-selectbox.component.tsx
@@ -8,17 +8,28 @@ export const SortBySelectBox = ({
   onChangeCallback: (changeEvent: ChangeEvent<HTMLSelectElement>) => void;
   disabled: boolean;
 }): React.ReactElement => {
-  const options = Object.keys(PropertiesSortBy).map((key: string) => (
-    <option value={PropertiesSortBy[key]}>{PropertiesSortBy[key]}</option>
-  ));
+  const options = Object.keys(PropertiesSortBy).map(
+    (key: string, index: number) => (
+      <option key={`sort-option-${index}`} value={PropertiesSortBy[key]}>
+        {PropertiesSortBy[key]}
+      </option>
+    )
+  );
   return (
-    <select
-      disabled={disabled}
-      id="properties-list-sortby"
-      onChange={onChangeCallback}
-      className="custom-select"
-    >
-      {options}
-    </select>
+    <div className="input-group">
+      <div className="input-group-append">
+        <label className="input-group-text" htmlFor="properties-list-sortby">
+          Sort by
+        </label>
+      </div>
+      <select
+        disabled={disabled}
+        id="properties-list-sortby"
+        onChange={onChangeCallback}
+        className="custom-select"
+      >
+        {options}
+      </select>
+    </div>
   );
 };
