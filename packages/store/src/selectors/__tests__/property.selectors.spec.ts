d922e86d0047ed770ce01f0cec1fcdf4cc6c0033
Giampiero Bono <giampiero.bono@hotmail.it>
2020-01-02 11:35:22 +0100

    test(store): add more tests for getSortedPropertiesList selector

--- packages/store/src/selectors/__tests__/property.selectors.spec.ts
+++ packages/store/src/selectors/__tests__/property.selectors.spec.ts
@@ -14,10 +14,58 @@ describe("Properties sort selectors", () => {
   });
 
   describe("getSortedPropertiesList", () => {
-    it("should return correct sorted list", () => {
+    it("should return correct sorted list by title", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.Title }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by rating", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.Rating }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by rating count", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.RatingCount }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by price", () => {
       const newMockState: GlobalState = {
         ...mockState,
         propertiesListSortBy: { sortConfig: PropertiesSortBy.Price }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by beds", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.Beds }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by baths", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.Bath }
+      };
+      expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
+    });
+
+    it("should return correct sorted list by sqFeets", () => {
+      const newMockState: GlobalState = {
+        ...mockState,
+        propertiesListSortBy: { sortConfig: PropertiesSortBy.SqFeets }
       };
       expect(getSortedPropertiesList(newMockState)).toMatchSnapshot();
     });
